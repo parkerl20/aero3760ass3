8ade8553c2c1202b744149e6bff57a7c883ac403
Lucas Parker <31122263+parkerl20@users.noreply.github.com>
2023-10-08 12:57:05 +1100

    Added some commenting

---
+++ 3-Attitude/mainAttitude.py
@@ -0,0 +1,10 @@
+'''
+
+Static or dynamic attitude determination
+
+Star Trackers onboard Satellite
+
+Two vectors: taken from unit vectors to two stars tracked by two star trackers for fine attitude determination
+We consider just the unit vectors, as the length of the vector has no information relevant to attitude determination
+
+'''
