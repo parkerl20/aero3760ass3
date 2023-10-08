ffe5041e16a8bb03ca9baf146e833a82907e2ff7
Lemuel-Noah-Fernandez <87133989+Lemuel-Noah-Fernandez@users.noreply.github.com>
2023-10-08 19:28:48 +1100

    GEE map works

---
+++ 5-GEE/initial_map.py
@@ -0,0 +1,44 @@
+import ee
+import geemap
+import folium
+import webbrowser
+import os
+
+# Initialise credentials
+service_account = "spacey@spacey-401400.iam.gserviceaccount.com"
+key_path = "5-GEE/key.json"    
+credentials = ee.ServiceAccountCredentials(service_account, key_path)
+ee.Initialize(credentials=credentials)
+
+
+# Set some points of interest
+# Mackay, Qld
+lat = -21.1434
+lon = 149.1868
+
+# Get URL for our image
+Data_Set_Clearing = ee.ImageCollection("WRI/GFW/FORMA/raw_output_firms")
+
+# Create an interactive map
+# Map = folium.Map(center=[lat, lon], zoom_start=7)
+Map = geemap.Map(center=[lat, lon], zoom=7)
+
+# Define the dataset
+dataset = ee.ImageCollection('WRI/GFW/FORMA/raw_output_firms')
+dataset.filter(ee.Filter.date('2018-08-01', '2018-08-15'))
+
+# Select the 'nday' band
+percentageOfClearing = dataset.select('nday')
+
+# Visualization parameters
+visParams = {
+'min': 0.0,
+'max': 0.01
+}
+
+# Add layer to map
+Map.addLayer(percentageOfClearing, visParams, 'Percentage of clearing')
+
+# Show the map
+Map.save('5-GEE/maps/initial_map.html')
+webbrowser.open('file:///' + os.path.realpath('5-GEE/maps/initial_map.html'))
