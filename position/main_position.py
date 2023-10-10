b8935d1c1025e45c45c65fa8c85f59468c8f0230
Daniel Mooney <daniel.mooney180@gmail.com>
2023-10-10 23:42:22 +1100

    implemented transition mtx func

--- position/main_position.py
+++ position/main_position.py
@@ -15,8 +15,41 @@ from spacesim import orbit_determination as od
 import numpy as np
 
 
-def transition_matrix_func(r: np.ndarray, t: float) -> np.ndarray:
-    pass
+def transition_matrix_func(r: np.ndarray, dt: float) -> np.ndarray:
+    mu = const.MU_EARTH
+    r_i, r_j, r_k = r.ravel()
+    r_mag = np.linalg.norm(r)
+    
+    # intermediate matrices
+    A = np.zeros((3, 3))
+    B = np.eye(3)
+    D = np.zeros((3, 3))
+    
+    c_00 = -(mu / r_mag**3) + (3 * mu * r_i**2 / r_mag**5)
+    c_01 = 3 * mu * r_i * r_j / r_mag**5
+    c_02 = 3 * mu * r_i * r_k / r_mag**5
+
+    c_10 = 3 * mu * r_j * r_i / r_mag**5
+    c_11 = -(mu / r_mag**3) + (3 * mu * r_j**2 / r_mag**5)
+    c_12 = 3 * mu * r_j * r_k / r_mag**5
+    
+    c_20 = 3 * mu * r_k * r_i / r_mag**5
+    c_21 = 3 * mu * r_k * r_j / r_mag**5
+    c_22 = -(mu / r_mag**3) + (3 * mu * r_k**2 / r_mag**5)
+    
+    C = np.array([
+        [c_00, c_01, c_02],
+        [c_10, c_11, c_12],
+        [c_20, c_21, c_22]
+    ])
+    
+    F0_inter = np.concatenate((A, B), axis=1)
+    F1_inter = np.concatenate((C, D), axis=1)
+    
+    F = np.concatenate((F0_inter, F1_inter), axis=0)
+    
+    trans_mtx = np.eye(6) + (F * dt) + (F @ F * (dt**2 / 2))
+    return trans_mtx
 
 def main() -> None:
     tle_file = 'tle.txt'
