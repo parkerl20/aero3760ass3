7d284528eb1635ba3c7fb8e85a3d4d39fdd613a3
Emily Sacks <emilysacks@Emilys-MacBook-Air.local>
2023-10-12 10:03:49 +1100

    added my nlls

--- attitude/nlls.py
+++ attitude/nlls.py
@@ -75,3 +75,77 @@ def nlls_gps(rho_obs,satellites):
         gdop = np.sqrt(V[0,0] + V[1,1] + V[2,2] + V[3,3])
 
     return pos_est, gdop
+
+
+
+
+def find_nlls_gs_estimate(pseudo_sat, pos_sat, pos_init):
+    """
+    Finds Non Linear Least Squares estimate of ground station position, and PDOP
+    Inputs:
+        pseudo_sat: pseudorange data for each satellite
+        pos_sat: ECI positions of satellite
+        pos_init: initial guess of ground station in ECI frame
+
+    Outputs:
+        pos_opt: final position estimate of ground station in LLH
+        pdop: PDOP for NLLSE
+        pos_store: All the computed position estimates over the iterations
+        i_nlls: Number of iterations for convergence
+    """
+    
+    # Initialise arrays
+    pos_store = np.array(pos_init)
+    fx = np.zeros_like(pseudo_sat)
+    dpdx = np.zeros_like(pseudo_sat)
+    dpdy = np.zeros_like(pseudo_sat)
+    dpdz = np.zeros_like(pseudo_sat)
+
+    # Initial iteration params
+    i_nlls = 0
+    max_iter = 200
+    tol =  1e-8
+    dpos = 100
+    pos_opt = pos_init
+
+    # until converges
+    while (i_nlls < max_iter) and (np.sum(np.abs(dpos) > tol)):
+        
+        # for each satellite find calculated range fx based on previous estimate
+        for i in range(0, pseudo_sat.size):
+            fx[i] = np.sqrt((pos_sat[i,0] - pos_store[0,i_nlls])**2 + (pos_sat[i, 1] - pos_store[1,i_nlls])**2\
+                            + (pos_sat[i,2] - pos_store[2,i_nlls])**2)
+            
+        # difference between measured and calculated
+        dp = pseudo_sat.T - fx.T
+
+        # Partial differentiation of p - building H matrix (without clock bias)
+        for i in range(0, pseudo_sat.size):
+            dpdx[i] = -(pos_sat[i,0] - pos_store[0,i_nlls]) / fx[i]
+            dpdy[i] = -(pos_sat[i,1] - pos_store[1,i_nlls]) / fx[i]
+            dpdz[i] = -(pos_sat[i,2] - pos_store[2,i_nlls]) / fx[i]
+
+
+        H = np.array([dpdx, dpdy, dpdz]).T
+        # Equal weights matrix
+        W = np.eye(pseudo_sat.size)
+        
+        # PDOP        
+        pdop = np.sqrt(np.trace( np.linalg.inv( H.T @ H )))
+
+        # NLLS calculation for change in position for next iterations
+        dpos = np.linalg.inv(H.T @ W @ H) @ H.T @ W @ dp
+        dpos = dpos.reshape((pos_opt.shape))
+        
+
+        if np.sum(np.abs(dpos) > tol):
+            pos_opt = pos_opt + dpos
+            i_nlls +=1
+            pos_store = np.append(pos_store, pos_opt, axis = 1)
+        else:
+            break
+    
+    print()
+    print(f"Number of iterations to converge: {i_nlls}")
+    print("PDOP:",pdop)
+    return pos_opt, pdop, pos_store, i_nlls
