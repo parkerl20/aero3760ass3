a9ca7f72d4db2d34d8f13521300f83f95a93bf23
Lemuel-Noah-Fernandez <87133989+Lemuel-Noah-Fernandez@users.noreply.github.com>
2023-10-12 11:40:12 +1100

    More maps added

--- 5-GEE/functions.py
+++ 5-GEE/functions.py
@@ -88,11 +88,17 @@ def initial_map():
 
     return Map
 
-def S2A():
+def S2A(start_date: str, end_date: str):
+    # Get the geometry of New South Wales
+    nsw = ee.FeatureCollection("FAO/GAUL/2015/level1").filter(
+            ee.Filter.eq('ADM1_NAME', 'New South Wales')
+    )
+ 
    # Sentinel-2A satellite
     dataset = (
         ee.ImageCollection('COPERNICUS/S2_SR')
-        .filterDate('2019-05-01', '2019-07-30')
+        .filterBounds(nsw)
+        .filterDate(start_date, end_date)
         # Pre-filter to get less cloudy granules.
         .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20))
         .map(mask_s2_clouds)
@@ -102,13 +108,75 @@ def S2A():
     visualization = {
         'min': 0.0,
         'max': 0.3,
-        # 'bands': ['B8', 'B4', 'B3'],
-        'bands': ['B4', 'B3', 'B2'],
+        'bands': ['B8', 'B4', 'B3'],
     }
 
     # Map initialisation
     Map = geemap.Map() 
     Map.set_center(146.9211, -31.2532, 6) # Center of nsw
     Map.add_ee_layer(dataset.mean(), visualization, 'Infrared')
+
+    return Map
+
+
+def elevation_5m():
+    # Get the geometry of New South Wales
+    nsw = ee.FeatureCollection("FAO/GAUL/2015/level1").filter(
+            ee.Filter.eq('ADM1_NAME', 'New South Wales')
+    )
+
+    # 5m elevation dataset
+    dataset = (
+        ee.ImageCollection('AU/GA/AUSTRALIA_5M_DEM')
+        .filterBounds(nsw)
+    )
+
+    # Visualisation parameter
+    elevation = dataset.select('elevation')
+    elevation_visualization = {
+        'min': 0.0,
+        'max': 150.0,
+        'palette': ['0000ff', '00ffff', 'ffff00', 'ff0000', 'ffffff'],
+    }
+
+    # Map initialisation
+    Map = geemap.Map() 
+    Map.addLayer(elevation, elevation_visualization, 'Elevation')
+
+    return Map
+
+
+def surface_temperature():
+    # Get the geometry of New South Wales
+    nsw = ee.FeatureCollection("FAO/GAUL/2015/level1").filter(
+            ee.Filter.eq('ADM1_NAME', 'New South Wales')
+    )
+
+    # 5m elevation dataset
+    dataset = (
+        ee.ImageCollection('JAXA/GCOM-C/L3/LAND/LST/V3')
+        .filterBounds(nsw)
+        .filterDate('2019-12-01', '2020-02-01')
+        .filter(ee.Filter.eq('SATELLITE_DIRECTION', 'D'))
+    )
+
+    # Multiply with slope coefficient
+    dataset = dataset.mean().multiply(0.02)
+
+    # Visualisation parameter
+    visualization = {
+        'bands': ['LST_AVE'],
+        'min': 250,
+        'max': 316,
+        'palette': ['040274','040281','0502a3','0502b8','0502ce','0502e6',
+                    '0602ff','235cb1','307ef3','269db1','30c8e2','32d3ef',
+                    '3be285','3ff38f','86e26f','3ae237','b5e22e','d6e21f',
+                    'fff705','ffd611','ffb613','ff8b13','ff6e08','ff500d',
+                    'ff0000','de0101','c21301','a71001','911003'],
+    }
+
+    # Map initialisation
+    Map = geemap.Map() 
+    Map.addLayer(dataset, visualization, 'Elevation')
 
     return Map
