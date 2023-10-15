b360e8886d5c1672d58237d00df639be4e0c9984
Daniel Mooney <daniel.mooney180@gmail.com>
2023-10-16 09:18:39 +1100

    sensor class mainly done

--- spacesim/sensor.py
+++ spacesim/sensor.py
@@ -1,3 +1,35 @@
-class Sensor():
-    def __init__(self) -> None:
-        pass
+from spacesim import orbit as orb
+from typing import Callable
+
+
+class SatelliteSensor():
+    """A class that can simulate a sensor on a satellite.
+    """
+    def __init__(
+        self,
+        simulator: Callable[['SatelliteSensor', orb.Orbit], None],
+        frequency: float = None
+    ) -> None:
+        """A sensor that works on a satellite
+
+        Args:
+            simulator (Callable[[SatelliteSensor, orb.Orbit], any]): A function that
+                simulates the sensor making measurements.
+            frequency (float): The operating frequency of the sensor. 
+                Defaults to None. 
+        """
+        self.simulator = simulator
+        self.mesurement: any = None
+        self.frequency = frequency
+        
+        return
+
+    def make_measurement(self, measurement: any) -> None:        
+        self.mesurement = measurement
+        return
+    
+    def get_measurement(self) -> any:
+        return self.mesurement
+    
+    def sampling_period(self) -> float:
+        return 1 / self.frequency
