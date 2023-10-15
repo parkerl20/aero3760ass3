bbb403d26571844def0eb7128425f537ab1d2d29
EmilyzeS <102123423+EmilyzeS@users.noreply.github.com>
2023-10-15 13:42:04 +1100

    having H matrix issues

--- attitude/attitude_emily.py
+++ attitude/attitude_emily.py
@@ -17,11 +17,6 @@ Planning for function
 
 
 # # reference vectors in LGCV
-# y = C_lgcv @ m
-
-# C_lgcv = np.array([[np.cos(psi)*np.cos(theta), np.sin(psi)*np.cos(theta), -np.sin(theta)],
-#               [np.cos(psi)*np.sin(theta)*np.sin(phi) - np.sin(psi)*np.cos(phi), np.sin(psi)*np.sin(theta)*np.sin(phi) + np.cos(psi)*np.cos(phi), np.cos(theta)*np.sin(phi)],
-#             [np.cos(psi)*np.sin(theta)*np.cos(phi) + np.sin(psi)*np.sin(phi), np.sin(psi)*np.sin(theta)*np.cos(phi) - np.cos(psi)*np.sin(phi), np.cos(theta)*np.cos(phi)]])
 
 # # for each vector measurement
 # # m is vector measurements
@@ -33,8 +28,12 @@ Planning for function
 
 
 
-def nlls_gps_weights(att_data, att_init):
+def nlls_gps_weights(vector_obs, ref_vectors_lgcv, att_init):
     """
+    known vectors ref_vectors_lgcv m
+    y measured vectors in body frame vector_obs
+    should be performing nlls on y which is in the body frame vectors
+
     Finds Non Linear Least Squares estimate of attitude in Euluer angles, and PDOP
     Inputs:
         
@@ -63,61 +62,98 @@ def nlls_gps_weights(att_data, att_init):
     max_iter = 200
     tol =  1e-8
     datt = 100
-    att_opt = np.array([att_init])
+    att_opt = att_init
 
     # until converges
     while (i < max_iter) and (np.sum(np.abs(datt) > tol)):
         
+        # for each sensor????
+        # how does this correspond to satellites from before
+        # like how do we get our input data???
+
         # for each angle find calculated  based on previous estimate
-        psi = np.deg2rad(att_opt[-1,0])
-        theta = np.deg2rad(att_opt[-1,1])
-        phi =  np.deg2rad(att_opt[-1,2])
+        psi = np.deg2rad(att_opt[0])
+        theta = np.deg2rad(att_opt[1])
+        phi =  np.deg2rad(att_opt[2])
+
+
+        C_lgcv = np.array([[np.cos(psi)*np.cos(theta), np.sin(psi)*np.cos(theta), -np.sin(theta)],
+                      [np.cos(psi)*np.sin(theta)*np.sin(phi) - np.sin(psi)*np.cos(phi), np.sin(psi)*np.sin(theta)*np.sin(phi) + np.cos(psi)*np.cos(phi), np.cos(theta)*np.sin(phi)],
+                    [np.cos(psi)*np.sin(theta)*np.cos(phi) + np.sin(psi)*np.sin(phi), np.sin(psi)*np.sin(theta)*np.cos(phi) - np.cos(psi)*np.sin(phi), np.cos(theta)*np.cos(phi)]])
+        
+        y = C_lgcv @ ref_vectors_lgcv.T
 
         # difference between measured and calculated
-        dy = att_data - att_opt
+        # this is wrong - basically i don't understand how do more iterations than i have data??
+        # look over other nlls again 
+        # should take all att data - 
+        dy = (vector_obs.T - y)
+        # i think this is wrong dimension
 
         print(dy)
+        print("dy", dy.shape)
 
-        # Building H matrix
+        # Building H matrix for each vector measurement
+        
+        H = []
+        for m in vector_obs:
+            H11 = 0
+            H12 = -np.cos(psi)*np.sin(theta)*m[0] - np.sin(psi)*np.sin(theta)*m[1] - np.cos(theta)*m[2]
+            H13 = -np.sin(psi)*np.cos(phi)*m[0] + np.cos(psi)*np.cos(phi)*m[1]
 
-        H11 = 0
-        H12 = -np.cos(psi)*np.sin(theta)*att_data[i,0] - np.sin(psi)*np.sin(theta)*att_data[i,1] - np.cos(theta)*att_data[i,2]
-        H13 = -np.sin(psi)*np.cos(phi)*att_data[i,0] + np.cos(psi)*np.cos(phi)*att_data[i,1]
+            H21 = (np.cos(psi)*np.sin(theta)*np.cos(phi) + np.sin(psi)*np.sin(phi))*m[0] + (np.sin(psi)*np.sin(theta)*np.cos(phi) - np.cos(psi)*np.sin(phi))*m[1] + np.cos(theta)*np.cos(phi)*m[2]
+            H22 = np.cos(psi)*np.cos(theta)*np.sin(phi)*m[0] + np.sin(psi)*np.cos(theta)*np.sin(phi)*m[1] - np.sin(theta)*np.sin(phi)*m[2]
+            H23 = (-np.sin(psi)*np.sin(theta)*np.sin(phi) - np.cos(psi)*np.cos(phi))*m[0] + (np.cos(psi)*np.sin(theta)*np.sin(phi) - np.sin(psi)*np.cos(phi))*m[1]
 
-        H21 = (np.cos(psi)*np.sin(theta)*np.cos(phi) + np.sin(psi)*np.sin(phi))*att_data[i,0] + (np.sin(psi)*np.sin(theta)*np.cos(phi) - np.cos(psi)*np.sin(phi))*att_data[i,1] + np.cos(theta)*np.cos(phi)*att_data[i,2]
-        H22 = np.cos(psi)*np.cos(theta)*np.sin(phi)*att_data[i,0] + np.sin(psi)*np.cos(theta)*np.sin(phi)*att_data[i,1] - np.sin(theta)*np.sin(phi)*att_data[i,2]
-        H23 = (-np.sin(psi)*np.sin(theta)*np.sin(phi) - np.cos(psi)*np.cos(phi))*att_data[i,0] + (np.cos(psi)*np.sin(theta)*np.sin(phi) - np.sin(psi)*np.cos(phi))*att_data[i,1]
+            H31 = (-np.cos(psi)*np.sin(theta)*np.sin(phi) + np.sin(psi)*np.cos(phi))*m[0] + (-np.sin(psi)*np.sin(theta)*np.sin(phi) - np.cos(psi)*np.cos(phi))*m[1] - np.cos(theta)*np.sin(phi)*m[2]
+            H32 = np.cos(psi)*np.cos(theta)*np.cos(phi)*m[0] + np.sin(psi)*np.cos(theta)*np.cos(phi)*m[1] - np.sin(theta)*np.cos(phi)*m[2]
+            H33 = (-np.sin(psi)*np.sin(theta)*np.cos(phi) + np.cos(psi)*np.sin(phi))*m[0] + (np.cos(psi)*np.sin(theta)*np.cos(phi) + np.sin(psi)*np.sin(phi))*m[1]
 
-        H31 = (-np.cos(psi)*np.sin(theta)*np.sin(phi) + np.sin(psi)*np.cos(phi))*att_data[i,0] + (-np.sin(psi)*np.sin(theta)*np.sin(phi) - np.cos(psi)*np.cos(phi))*att_data[i,1] - np.cos(theta)*np.sin(phi)*att_data[i,2]
-        H32 = np.cos(psi)*np.cos(theta)*np.cos(phi)*att_data[i,0] + np.sin(psi)*np.cos(theta)*np.cos(phi)*att_data[i,1] - np.sin(theta)*np.cos(phi)*att_data[i,2]
-        H33 = (-np.sin(psi)*np.sin(theta)*np.cos(phi) + np.cos(psi)*np.sin(phi))*att_data[i,0] + (np.cos(psi)*np.sin(theta)*np.cos(phi) + np.sin(psi)*np.sin(phi))*att_data[i,1]
+            # H = np.append(H, [
+            # H = np.array([
+            H.append(np.array([
+                [H11, H12, H13 ],
+                [H21, H22, H23],
+                [H31, H32, H33]]))
+        
+        H = np.array(H)
+        print(H.shape)
+        H = H.reshape(vector_obs.size,3)
+        # make H for each sensor
+        # H doesn't work because needs to be square
+        # H = np.array([H,H,H])
 
-        H = np.array([
-            [H11, H12, H13 ],
-            [H21, H22, H23],
-            [H31, H32, H33]])
 
 
+        # Equal weights matrix for 
+        W = np.eye(vector_obs.size)
+        print(vector_obs.shape)
 
-        # Equal weights matrix
-        W = np.eye(att_data.size)
-        W = np.eye((3))
+        # W = np.eye(3)
         
         print(H.shape)
         # PDOP        
         pdop = np.sqrt(np.trace( np.linalg.inv( H.T @ H )))
 
         # NLLS calculation for change in attitude for next iterations
+
+
+        # H = np.array([H,H,H])
+
+        print((np.linalg.inv(H.T @ W @ H) @ H.T @ W).shape)
+        print(dy.shape)
         datt = np.linalg.inv(H.T @ W @ H) @ H.T @ W @ dy
 
         # confused about dimension sizing
-        datt = datt.reshape((att_opt.shape))
+        # datt = datt.reshape((att_opt.shape))
         
 
         if np.sum(np.abs(datt) > tol):
-            att_opt = att_opt + datt
+            att_opt = att_opt + datt.T
             i +=1
-            att_store = np.append(att_store, att_opt, axis = 1)
+            print((att_opt).flatten().shape)
+            print(att_store.shape)
+            att_store = np.append(att_store, att_opt.flatten(), axis = 0)
         else:
             break
     
@@ -129,6 +165,13 @@ def nlls_gps_weights(att_data, att_init):
 
 
 att_init = (np.array([11,32,-45]))
+
+# say we have 4 sensors
 att_data = np.array([[10,31,-41],[9,30,-47],[14,32,-44]])
+
+vector_obs = np.array([[-0.0879, 0.5242, -0.6383],[-0.3319, 0.3281, 0.2055], [0.8465, 0.0540, 0.6485]])
+ref_vectors_lgcv = np.array([[0.2,0.7,-0.4],[0.1,0.3,0.4],[0.7,-0.8,0.1] ])
 # needs to be in radians
-nlls_gps_weights(att_data, att_init)
+nlls_gps_weights(vector_obs, ref_vectors_lgcv, att_init)
+
+# justifying nlls
