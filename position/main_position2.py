ef70dc3d68491036904cd19316e9be6b596ff21c
Daniel Mooney <daniel.mooney180@gmail.com>
2023-10-19 12:42:41 +1100

    moved main position

--- position/main_position2.py
+++ position/main_position2.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+current_file_path = os.path.abspath(__file__)
+parent_directory = os.path.dirname(current_file_path)
+grandparent_directory = os.path.dirname(parent_directory)
+sys.path.append(grandparent_directory)
+
 from spacesim import satellite as sat
 from spacesim import celestial_body as cb
 from spacesim import constants as const
@@ -6,6 +14,7 @@ from spacesim import sensor
 
 import numpy as np
 import datetime as dt
+import matplotlib.pyplot as plt
 
 def main() -> None:
     pass
