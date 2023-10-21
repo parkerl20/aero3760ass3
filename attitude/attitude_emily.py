9392d76a75bd693ad8ee1efbc319e6761bb2287c
EmilyzeS <102123423+EmilyzeS@users.noreply.github.com>
2023-10-21 14:10:09 +1100

    changed lgcv matrix to quaternions

--- attitude/attitude_emily.py
+++ attitude/attitude_emily.py
@@ -1,5 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.spatial.transform import Rotation
+
+
+
+# quaternions in scipy
 
 '''
 
@@ -24,9 +29,7 @@ Planning for function
 
 
 
-
-
-def nlls_gps_weights(vector_obs, ref_vectors_lgcv, att_init):
+def nlls_quaternions_weights(vector_obs, ref_vectors_lgcv, att_init):
     """
     known vectors ref_vectors_lgcv m
     y measured vectors in body frame vector_obs
@@ -58,7 +61,7 @@ def nlls_gps_weights(vector_obs, ref_vectors_lgcv, att_init):
     # Initial iteration params
     i = 0
     max_iter = 100
-    tol =  1e-8
+    tol =  1e-2
     datt = 100
     att_opt = att_init
     datts = []
@@ -82,13 +85,26 @@ def nlls_gps_weights(vector_obs, ref_vectors_lgcv, att_init):
         
         y = C_lgcv @ ref_vectors_lgcv.T
 
-        # difference between measured and calculated
-        dy = (vector_obs.T - y)
+        #y is vectors y[:,i] is a vector 
 
-        print(dy)
+        print(y.T)
+
+        # difference between measured and calculated
+        dy = (vector_obs - y.T)
+
+        print(y.T)
+        print("vector obs", vector_obs)
+        print("dy",dy)
+
+
+
         print("dy", dy.shape)
         dy = dy.reshape(dy.size)
 
+
+        print(dy)
+
+        
         # Building H matrix for each vector measurement
         
         H = []
@@ -121,6 +137,29 @@ def nlls_gps_weights(vector_obs, ref_vectors_lgcv, att_init):
 
        # Equal weights matrix for 
         W = np.eye(vector_obs.size)
+     
+        # W = np.array([
+        #     [0.2,0,0,0,0,0,0,0,0,0,0,0],
+        #     [0,0.2,0,0,0,0,0,0,0,0,0,0],
+        #     [0,0,0.2,0,0,0,0,0,0,0,0,0],
+        #     [0,0,0,1,0,0,0,0,0,0,0,0],
+        #     [0,0,0,0,1,0,0,0,0,0,0,0],
+        #     [0,0,0,0,0,1,0,0,0,0,0,0],
+        #     [0,0,0,0,0,0,1,0,0,0,0,0],
+        #     [0,0,0,0,0,0,0,1,0,0,0,0],
+        #     [0,0,0,0,0,0,0,0,1,0,0,0],
+        #     [0,0,0,0,0,0,0,0,0,1,0,0],
+        #     [0,0,0,0,0,0,0,0,0,0,1,0],
+        #     [0,0,0,0,0,0,0,0,0,0,0,1]
+
+        # ])
+
+        W[0,0] = 0.2
+        W[1,1] = 0.2
+        W[2,2] = 0.2
+        
+
+
         # print(vector_obs.shape)
 
         # W = np.eye(3)
@@ -158,6 +197,268 @@ def nlls_gps_weights(vector_obs, ref_vectors_lgcv, att_init):
     print("PDOP:",pdop)
     return att_opt, pdop, att_store, i, datts
 
+def euler2quat(x):
+    """
+    z,y,x
+    yaw, pitch, roll
+    psi, theta, phi
+    """
+
+    yaw = np.deg2rad(x[0])
+    pitch = np.deg2rad(x[1])
+    roll = np.deg2rad(x[2])
+    qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+    qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+    qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    
+    return np.array([ qx, qy, qz,qw])
+
+
+# print(euler2quat(0,90,0))
+# print(euler2quat(90,0,0))
+# print(euler2quat(90,45,30))
+
+
+def quat2euler(q):
+    # roll (x-axis rotation)
+    #x,y,z,w
+    (x, y, z, w) = (q[0], q[1], q[2], q[3])
+
+    t0 = 2 * (w * x + y * z)
+    t1 = 1 - 2 * (x * x + y * y)
+    roll = np.arctan2(t0, t1)
+    t2 = 2 * (w * y - z * x)
+    t2 = 1 if t2 > 1 else t2
+    t2 = -1 if t2 < -1 else t2
+    pitch = np.arcsin(t2)
+    t3 = 2 * (w * z + x * y)
+    t4 = 1 - 2 * (y * y + z * z)
+    yaw = np.arctan2(t3, t4)
+
+
+    yaw_deg = np.rad2deg(yaw)
+    pitch_deg = np.rad2deg(pitch)
+    roll_deg = np.rad2deg(roll)
+    return np.array([yaw_deg, pitch_deg, roll_deg])
+
+# print(quat2euler(euler2quat(90,45,30)))
+# print(quat2euler(euler2quat(0,-45,30)))
+
+# print(quat2euler(np.array([-0.09229596 , 0.43045933,  0.56098553 , 0.70105738])))
+# yo
+
+def nlls_euler_weights(vector_obs, ref_vectors_lgcv, att_init):
+    """
+    known vectors ref_vectors_lgcv m
+    y measured vectors in body frame vector_obs
+    should be performing nlls on y which is in the body frame vectors
+
+    Finds Non Linear Least Squares estimate of attitude in Euluer angles, and PDOP
+    Inputs:
+        
+        att_data: attitude data for each satellite in Euluer (we can change this to be lgcv or something using matrix above)
+        
+        att_init: initial guess of attitude of sat in Euler angles
+
+    Outputs:
+        pos_opt: final position estimate of ground station in LLH
+        pdop: PDOP for NLLSE
+        pos_store: All the computed position estimates over the iterations
+        i_nlls: Number of iterations for convergence
+    """
+    
+    # Initialise arrays
+    att_store = np.array([att_init])
+
+
+    # fx = np.zeros_like(pseudo_sat)
+    # dpdx = np.zeros_like(pseudo_sat)
+    # dpdy = np.zeros_like(pseudo_sat)
+    # dpdz = np.zeros_like(pseudo_sat)
+
+    # Initial iteration params
+    i = 0
+    max_iter = 100
+    tol =  1e-1
+    datt = 100
+    # initial guess in quaternion
+    att_opt = euler2quat(att_init)
+    # att_opt = att_init
+    datts = []
+
+    # until converges
+    while (i < max_iter) and (np.sum(np.abs(datt) > tol)):
+        
+        # for each sensor????
+        # how does this correspond to satellites from before
+        # like how do we get our input data???
+
+        # for each angle find calculated  based on previous estimate
+        qx = att_opt[0]
+        qy = att_opt[1]
+        qz = att_opt[2]
+        qw = att_opt[3]
+
+        # x_eul = quat2euler(att_opt)
+        # psi = x_eul[0]
+        # theta = x_eul[1]
+        # phi = x_eul[2]
+        # psi = np.deg2rad(att_opt[0])
+        # theta = np.deg2rad(att_opt[1])
+        # phi = np.deg2rad(att_opt[2])
+
+
+
+        # C_lgcv2body = np.array([[np.cos(psi)*np.cos(theta), np.sin(psi)*np.cos(theta), -np.sin(theta)],
+        #               [np.cos(psi)*np.sin(theta)*np.sin(phi) - np.sin(psi)*np.cos(phi), np.sin(psi)*np.sin(theta)*np.sin(phi) + np.cos(psi)*np.cos(phi), np.cos(theta)*np.sin(phi)],
+        #             [np.cos(psi)*np.sin(theta)*np.cos(phi) + np.sin(psi)*np.sin(phi), np.sin(psi)*np.sin(theta)*np.cos(phi) - np.cos(psi)*np.sin(phi), np.cos(theta)*np.cos(phi)]])
+        
+
+        # # change this to quaternions
+        C_lgcv2body_quat = np.array([[qw**2+qx**2-qy**2-qz**2, 2*(qx*qy-qw*qz), 2*(qx*qz-qw*qy)],
+                                [2*(qx*qy+qw*qz), qw**2*qx**2+qy**2+qz**2, 2*(qy*qz-qw*qx)],
+                                [2*(qx*qz-qw*qy), 2*(qy*qz+qw*qx), qw**2-qx**2-qy**2+qz**2]])
+
+        # y = C_lgcv @ ref_vectors_lgcv.T
+        y = C_lgcv2body_quat @ ref_vectors_lgcv.T
+        # print("y", y)
+
+        # y is in body frame
+        # we make some observation in body frame
+        # then we have some reference vectors in lgcv
+        # we use the angles we have to find the equivalent body frame measurements
+
+        #y is vectors y[:,i] is a vector 
+
+        # print("y", y.T)
+
+        # difference between measured and calculated
+        dy = (vector_obs - y.T)
+
+        # print(y.T)
+        # print("vector obs", vector_obs)
+        # print("dy",dy)
+
+
+
+        # print("dy", dy.shape)
+        dy = dy.reshape(dy.size)
+
+
+        # print(dy)
+
+        
+        # # Building H matrix for each vector measurement
+        # H is derivative of C_lgcv2body matrix
+        # 𝑄(𝑞∘𝑝)⋅𝐼∗ + 𝑄̂ (𝑝∘𝑞−1)
+ 
+
+        H = []
+        for m in vector_obs:
+            I_star = np.diag(np.diag([1,-1,-1,-1]))
+            H_quat = C_lgcv2body_quat*I_star + 
+            H.append(np.array())
+            
+            f
+            # H11 = 
+
+
+
+
+        # # # For EULER:
+        # H = []
+        # for m in vector_obs:
+        #     H11 = 0
+        #     H12 = -np.cos(psi)*np.sin(theta)*m[0] - np.sin(psi)*np.sin(theta)*m[1] - np.cos(theta)*m[2]
+        #     H13 = -np.sin(psi)*np.cos(phi)*m[0] + np.cos(psi)*np.cos(phi)*m[1]
+
+        #     H21 = (np.cos(psi)*np.sin(theta)*np.cos(phi) + np.sin(psi)*np.sin(phi))*m[0] + (np.sin(psi)*np.sin(theta)*np.cos(phi) - np.cos(psi)*np.sin(phi))*m[1] + np.cos(theta)*np.cos(phi)*m[2]
+        #     H22 = np.cos(psi)*np.cos(theta)*np.sin(phi)*m[0] + np.sin(psi)*np.cos(theta)*np.sin(phi)*m[1] - np.sin(theta)*np.sin(phi)*m[2]
+        #     H23 = (-np.sin(psi)*np.sin(theta)*np.sin(phi) - np.cos(psi)*np.cos(phi))*m[0] + (np.cos(psi)*np.sin(theta)*np.sin(phi) - np.sin(psi)*np.cos(phi))*m[1]
+
+        #     H31 = (-np.cos(psi)*np.sin(theta)*np.sin(phi) + np.sin(psi)*np.cos(phi))*m[0] + (-np.sin(psi)*np.sin(theta)*np.sin(phi) - np.cos(psi)*np.cos(phi))*m[1] - np.cos(theta)*np.sin(phi)*m[2]
+        #     H32 = np.cos(psi)*np.cos(theta)*np.cos(phi)*m[0] + np.sin(psi)*np.cos(theta)*np.cos(phi)*m[1] - np.sin(theta)*np.cos(phi)*m[2]
+        #     H33 = (-np.sin(psi)*np.sin(theta)*np.cos(phi) + np.cos(psi)*np.sin(phi))*m[0] + (np.cos(psi)*np.sin(theta)*np.cos(phi) + np.sin(psi)*np.sin(phi))*m[1]
+
+        #     # H = np.append(H, [
+        #     # H = np.array([
+        #     H.append(np.array([
+        #         [H11, H12, H13 ],
+        #         [H21, H22, H23],
+        #         [H31, H32, H33]]))
+        
+        # H = np.array(H)
+        # print(H.shape)
+        # Reshaping to be 2 dimensional
+        H = H.reshape(vector_obs.size,3)
+        # make H for each sensor
+        # H = np.array([H,H,H])
+
+       # Equal weights matrix for 
+        W = np.eye(vector_obs.size)
+     
+        # W = np.array([
+        #     [0.2,0,0,0,0,0,0,0,0,0,0,0],
+        #     [0,0.2,0,0,0,0,0,0,0,0,0,0],
+        #     [0,0,0.2,0,0,0,0,0,0,0,0,0],
+        #     [0,0,0,1,0,0,0,0,0,0,0,0],
+        #     [0,0,0,0,1,0,0,0,0,0,0,0],
+        #     [0,0,0,0,0,1,0,0,0,0,0,0],
+        #     [0,0,0,0,0,0,1,0,0,0,0,0],
+        #     [0,0,0,0,0,0,0,1,0,0,0,0],
+        #     [0,0,0,0,0,0,0,0,1,0,0,0],
+        #     [0,0,0,0,0,0,0,0,0,1,0,0],
+        #     [0,0,0,0,0,0,0,0,0,0,1,0],
+        #     [0,0,0,0,0,0,0,0,0,0,0,1]
+
+        # ])
+
+        W[0,0] = 0.5
+        W[1,1] = 0.5
+        W[2,2] = 0.5
+        
+
+
+        # print(vector_obs.shape)
+
+        # W = np.eye(3)
+        
+        # print(H.shape)
+        # PDOP        
+        pdop = np.sqrt(np.trace( np.linalg.inv( H.T @ H )))
+
+        # NLLS calculation for change in attitude for next iterations
+        # H = np.array([H,H,H])
+        # print(dy)
+        datt = np.linalg.inv(H.T @ W @ H) @ H.T @ W @ dy
+
+        # this will  output radians
+        # print(datt)
+
+        datt_deg = np.rad2deg(datt)
+        # print(datt_deg)
+
+        # confused about dimension sizing
+        # datt = datt.reshape((att_opt.shape))
+        
+
+        if np.sum(np.abs(datt) > tol):
+            print(att_opt)
+            att_opt = att_opt + datt.T
+            print(datt)
+            datts.append(np.abs(np.linalg.norm(datt)))
+            print(att_opt)
+            i +=1
+            att_store = np.append(att_store,[att_opt], axis = 0)
+        else:
+            break
+    
+    print()
+    print(f"Number of iterations to converge: {i}")
+    print("PDOP:",pdop)
+    return att_opt, pdop, att_store, i, datts
+
 
 
 att_init = (np.array([10,32,-45]))
@@ -165,10 +466,18 @@ att_init = (np.array([10,32,-45]))
 # att_data = np.array([[10,31,-41],[9,30,-47],[14,32,-44]])
 
 # say we have 4 sensors
-vector_obs = np.array([[-0.0879, 0.5242, -0.6383],[-0.3319, 0.3281, 0.2055], [0.8465, 0.0540, 0.6485],[0.8465, 0.0540, 0.6485]])
-ref_vectors_lgcv = np.array([[0.2,0.7,-0.4],[0.1,0.3,0.4],[0.7,-0.8,0.1],[0.7,-0.8,0.1] ])
+# this is in lgcv??
+vector_obs = np.array([[ 0.3,  0.5,  0.3],[-0.08, -0.01,   0.5], [ 0.4, -0.9, -0.4],[ 0.4, -0.9, -0.3]])
+vector_obs = np.array([[-0.3, 0.3, -0.5], [-0.4, -0.2, -0.01], [-0.2, 0.5, 0.9], [-0.2, -0.5, 0.9]])
+vector_obs = np.array([[-0.0879, 0.5242, -0.6383],[-0.3319, 0.3281, 0.2055], [0.8465, 0.0540, 0.6485]])
+
+# [[ 0.48208398  0.58341093  0.34238388]
+#  [-0.08427267 -0.0996557   0.49291668]
+#  [ 0.41381351 -0.90926659 -0.37681912]
+#  [ 0.41381351 -0.90926659 -0.37681912]]
+ref_vectors_lgcv = np.array([[0.2,0.7,-0.4],[0.1,0.3,0.4],[0.7,-0.8,0.1] ])
 # needs to be in radians
-att_opt, pdop, att_store, i, datts = nlls_gps_weights(vector_obs, ref_vectors_lgcv, att_init)
+att_opt, pdop, att_store, i, datts = nlls_euler_weights(vector_obs, ref_vectors_lgcv, att_init)
 
 print("Final attitude estimation", att_opt)
 print(att_store.shape)
@@ -191,3 +500,25 @@ plt.show()
 
 
 # justifying nlls
+
+
+
+"""
+Tasks TODO
+
+add quaternions
+run with jacobian quaternions
+
+get to converge
+test with different weights
+
+figure out how this interacts: what data do i need
+what actually makes sense
+make some sample data
+integrate with position
+
+make nice code (obj oriented)
+
+
+
+"""
