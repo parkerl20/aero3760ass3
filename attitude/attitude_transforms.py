67acac186e7aa445d992d1c0c8f6d7337e5e20de
EmilyzeS <102123423+EmilyzeS@users.noreply.github.com>
2023-10-30 01:56:52 +1100

    noramlise quaternions

--- attitude/attitude_transforms.py
+++ attitude/attitude_transforms.py
@@ -18,8 +18,9 @@ def euler2quat(x):
     qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
     qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
     qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-    
-    return np.array([ qw,qx, qy, qz])
+    quat = np.array([ qw,qx, qy, qz])
+    quat = quat/np.linalg.norm(quat)
+    return quat
 
 
 def quat2euler(q):
